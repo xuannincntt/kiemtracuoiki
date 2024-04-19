8894d832fab5426ae333b8c5057cbb246eb6781e
xuannincntt <xuannincntt@gmail.com>
2024-04-19 21:23:52 +0700

    19/4

---
+++ js/cau1.js
@@ -0,0 +1,7 @@
+function ham1() {
+    var nds = document.getElementByid("nd");
+    var its = document.getElementByClassName("it");
+    var signs = document.getElementByid("sign");
+    its[0].style.display = "none";
+    nds.style.display = "block";
+}
